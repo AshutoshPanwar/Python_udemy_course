33c8fedb77e8487213f381917edb2df028108cf3
ashudev <ashupanwar1100@gmail.com>
2021-04-24 17:33:19 +0530

    complex numners

---
+++ complex_numbers.py
@@ -0,0 +1,6 @@
+x = 2 + 3j
+
+print(type(x))                  # class"complex"
+
+print(x.real)                   # will display the real value 2.0 (it can be of any type)
+print(x.imag)                   # will display imagenary part 3.0 (it can only be integer or floting number)
