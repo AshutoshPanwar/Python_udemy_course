29bef0412397d0356c69a9a6297aa957b8bbe8ad
ashudev <ashupanwar1100@gmail.com>
2021-04-24 22:46:28 +0530

    positive indexing and negative indexing[]

---
+++ string_indexing.py
@@ -0,0 +1,8 @@
+s = "hello python"
+
+print(s[0])     #output: h indexing starts form 0 and so on.
+print(s[4])     #output: o 
+
+print(s[-1])    #output: n negative index starts form 1 and so on.
+
+print(s[-25])   #type error
