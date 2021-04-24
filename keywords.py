b8e3ed591cfe98e692a1dabe06572a34aa2ebf94
ashudev <ashupanwar1100@gmail.com>
2021-04-24 15:10:50 +0530

    keywords

---
+++ keywords.py
@@ -0,0 +1,5 @@
+# keywords are reversed words in python and have a predefined meaning in python language (there are 33 keywords)
+
+import keyword              #this will import keyword library
+
+print(keyword.kwlist)       # it will list all the keywords
