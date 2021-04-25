7fedf5c41fb16dd7607e304cfe5f54a364eccc43
ashudev <ashupanwar1100@gmail.com>
2021-04-25 18:17:31 +0530

    arithmetic operators +,-,*,%,**,/

---
+++ arithmetic_operators.py
@@ -0,0 +1,16 @@
+#in total there are 7 arithmetic operators in python
+print(3 + 4)    #adding two integers will give ans in integer
+print(2.5 * 2)  #operation between float or int will always give us float numbers
+print(2 - 15)   #ans would be an integer
+
+#Division(/) operator result is always float..
+print(10 / 2)   #output: 5.0 which is float
+
+#Modulus(%) operator will returns the remainder in integer or float value
+print(14 % 4)   #3*4+2 = 2 remainder
+print(14.75 % 4)#3*4+2.75 = 2.75 remainde
+
+#Exponential(**) operator become power of the number
+print(2 ** 3)   #2*2*2 = 8
+print(3.5 ** 2) #3.5*3.5 = 12.25
+
