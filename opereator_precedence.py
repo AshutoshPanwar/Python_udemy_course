c22e002b796b0c233511164e6a3591d14f511f9e
ashudev <ashupanwar1100@gmail.com>
2021-04-26 22:51:57 +0530

    Precedence of Operators

---
+++ opereator_precedence.py
@@ -0,0 +1,11 @@
+print( 3 + 4 * 5)       #precidence of * > + so we get 23
+
+print((3 + 4) * 5)      #precidence of () > * > + so get 35
+
+a = 30 
+b = 20 
+c = 10
+d = 5
+
+z = (a + b) * c / d     #precedence of () > * = / so we get 100.0
+print(z)
