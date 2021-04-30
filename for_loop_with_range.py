bf644886a3db76d9ae376687a811ada9ea68e9f8
ashudev <ashupanwar1100@gmail.com>
2021-04-30 13:27:28 +0530

    always use for loop with range

---
+++ for_loop_with_range.py
@@ -0,0 +1,23 @@
+x = range(5)
+print(x)
+
+for i in x:
+    print(i)
+
+x = range(3,9)
+print(x)
+
+for i in x:
+    print(i)
+
+x = range(0,10,2)
+print(x)
+
+for i in x:
+    print(i)
+
+x = range(10,0,-1)
+print(x)
+
+for i in x:
+    print(i)
