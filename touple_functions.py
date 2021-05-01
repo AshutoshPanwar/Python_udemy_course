98e4a0913dd1d7eb0d348d0d1a1dc0ce65e214e0
ashudev <ashupanwar1100@gmail.com>
2021-05-01 23:40:33 +0530

    .sum(),.min(),.max()

---
+++ touple_functions.py
@@ -0,0 +1,16 @@
+t = (10, 43, 52.2, 34,21,63)
+t2 = (10, 43, 52.2, 34,21,63)
+
+print(len(t))
+
+print(sum(t))
+
+print(min(t))
+
+print(max(t))
+
+#we can't use the sort method because tuples are immutable
+#we can't use the reverse method because tuples are immutable n 
+
+print(43 in t)
+print(t == t2)
