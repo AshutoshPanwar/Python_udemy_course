319e45466f24bb28f73292ef65850e424b2227c4
ashudev <ashupanwar1100@gmail.com>
2021-05-01 18:01:58 +0530

    .sum(),.min(),.max(),_in list

---
+++ list_functions,membership,identity.py
@@ -0,0 +1,11 @@
+l = [10, 54, 2, 61, 15]
+
+print(sum(l))   #will give the sum of list
+
+print(min(l))   #will give the minimum value in list
+
+print(max(l))   #will give maximum value in list
+
+print(54 in l)  #to check weather the number is present in a list or not
+
+print(32 not in l)
