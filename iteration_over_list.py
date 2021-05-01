d039100edbf4734c7f381cf5329d3d7192c123b6
ashudev <ashupanwar1100@gmail.com>
2021-05-01 18:12:20 +0530

    for and while loop on list

---
+++ iteration_over_list.py
@@ -0,0 +1,15 @@
+l = [10,54,2,61,15]
+
+for i in l:
+    print(i)
+
+for i in range(10):
+    print(i)
+
+for i in range(5):
+    print(i,l[i])
+
+i = 0
+while i  < len(l):
+    print(i,l[i])
+    i = i + 1
